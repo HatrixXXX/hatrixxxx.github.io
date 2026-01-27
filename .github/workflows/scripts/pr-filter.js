d242ab06a6b9a759b24ce54a1af32075a772cc99
HatrixXXXXX <1120223467@bit.edu.cn>
2026-01-27 09:20:23 +0800

    migrate

---
+++ .github/workflows/scripts/pr-filter.js
@@ -0,0 +1,48 @@
+function hasTypes(markdown) {
+  return /## Type of change/.test(markdown) && /-\s\[x\]/i.test(markdown);
+}
+
+function hasDescription(markdown) {
+  return (
+    /## Description/.test(markdown) &&
+    !/## Description\s*\n\s*(##|\s*$)/.test(markdown)
+  );
+}
+
+function removeHtmlComments(input) {
+  let prev;
+  do {
+    prev = input;
+    input = input.replace(/<!--[\s\S]*?-->/g, '');
+  } while (input !== prev);
+  return input;
+}
+
+module.exports = async ({ github, context, core }) => {
+  const pr = context.payload.pull_request;
+  const body = pr.body === null ? '' : pr.body;
+
+  // ✅ 修复 CodeQL 报警点
+  const markdown = removeHtmlComments(body);
+
+  const action = context.payload.action;
+
+  const isValid =
+    markdown !== '' && hasTypes(markdown) && hasDescription(markdown);
+
+  if (!isValid) {
+    await github.rest.pulls.update({
+      ...context.repo,
+      pull_number: pr.number,
+      state: 'closed'
+    });
+
+    await github.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pr.number,
+      body: `Oops, it seems you've ${action} an invalid pull request. No worries, we'll close it for you.`
+    });
+
+    core.setFailed('PR content does not meet template requirements.');
+  }
+};
---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# source "https://rubygems.org"
+source "https://gems.ruby-china.com/"
+
+gem "jekyll-theme-chirpy", "~> 7.2", ">= 7.2.4"
+gem "html-proofer", "~> 5.2.0", group: :test
+gem "logger"
+
+platforms :windows do
+  gem "tzinfo", ">= 1", "< 3"
+  gem "tzinfo-data"
+end
+
+gem "wdm", "~> 0.2.0", :platforms => [:windows]
