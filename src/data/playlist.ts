124fe2449c91c7c0559a8e629bf9c348b77aa581
HatrixXXX <15340528156@163.com>
2026-09-06 14:09:31 +0800

    feat: add local test playlist tracks

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -1,3 +1,22 @@
 import type { Track } from '../types/content';
 
-export const playlist: readonly Track[] = [];
+export const playlist: readonly Track[] = [
+  {
+    id: 'xue-zhiqian-qishi',
+    title: '其实',
+    artist: '薛之谦',
+    src: '/audio/xue-zhiqian-qishi.mp3'
+  },
+  {
+    id: 'xue-zhiqian-tianwailaiwu',
+    title: '天外来物',
+    artist: '薛之谦',
+    src: '/audio/xue-zhiqian-tianwailaiwu.mp3'
+  },
+  {
+    id: 'xue-zhiqian-yanyuan',
+    title: '演员',
+    artist: '薛之谦',
+    src: '/audio/xue-zhiqian-yanyuan.mp3'
+  }
+];
